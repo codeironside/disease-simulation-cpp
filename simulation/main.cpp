bfc1de4a37de82588bcd67db4e35f3923df70778
Sulaiman Hassan <sulaiman.hassan@stud.th-deg.de>
2024-07-01 20:02:25 +0200

    Update main.cpp

--- simulation/main.cpp
+++ simulation/main.cpp
@@ -1,10 +1,29 @@
+// #include <iostream>
+// #include "simulation.h"
+// #include <string>
+// int main() {
+//     std::cout << "Loading Disease Simulation..." << std::endl;
+//     Simulation sim = Simulation();
+//     sim.start();
+//     return 0;
+// }
+
 #include <iostream>
+#include <mpi.h>
 #include "simulation.h"
 #include <string>
-int main() {
-    std::cout << "Loading Disease Simulation..." << std::endl;
+
+int main(int argc, char *argv[]) {
+    MPI_Init(&argc, &argv);
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    if (rank == 0) {
+        std::cout << "Loading Disease Simulation on process " << rank << "..." << std::endl;
+    }
     Simulation sim = Simulation();
     sim.start();
+    MPI_Finalize();
     return 0;
 }
+
 

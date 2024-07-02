2950193e5bb0e61f94c68e53994a813f90762be1
Sulaiman Hassan <sulaiman.hassan@stud.th-deg.de>
2024-07-02 16:32:20 +0200

    Update main.cpp

--- simulation/main.cpp
+++ simulation/main.cpp
@@ -1,13 +1,3 @@
-// #include <iostream>
-// #include "simulation.h"
-// #include <string>
-// int main() {
-//     std::cout << "Loading Disease Simulation..." << std::endl;
-//     Simulation sim = Simulation();
-//     sim.start();
-//     return 0;
-// }
-
 #include <iostream>
 #include <mpi.h>
 #include "simulation.h"

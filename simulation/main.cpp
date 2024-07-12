d5659d2322319456c66d847a77a00ac3819173af
Sulaiman Hassan <sulaiman.hassan@stud.th-deg.de>
2024-07-12 13:58:19 +0200

    Update main.cpp

--- simulation/main.cpp
+++ simulation/main.cpp
@@ -17,25 +17,12 @@
 // }
 
 #include <iostream>
-#include <mpi.h>
 #include "simulation.h"
 #include <string>
-
-int main(int argc, char *argv[]) {
-    MPI_Init(&argc, &argv);
-    int rank;
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
-    std::string input_file = "disease_in.ini"; // Adjust as per your configuration
-    Simulation sim(input_file);
-
-    if (rank == 0) {
-        std::cout << "Loading Disease Simulation on process " << rank << "..." << std::endl;
-    }
-
+int main()
+{
+    std::cout << "Loading Disease Simulation..." << std::endl;
+    Simulation sim = Simulation();
     sim.start();
-
-    MPI_Finalize();
     return 0;
 }
-

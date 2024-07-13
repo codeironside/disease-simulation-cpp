5b7bf8e8d6a93a5e5fe69f2dd79cba9d5189befa
Sulaiman Hassan <sulaiman.hassan@stud.th-deg.de>
2024-07-13 03:09:44 +0200

    Update main.cpp

--- simulation/task two/main.cpp
+++ simulation/task two/main.cpp
@@ -1,11 +1,36 @@
 
 #include <iostream>
 #include "simulation.h"
+#include <string>#include <iostream>
+#include <mpi.h>
+#include "simulation.h"
 #include <string>
-int main()
+
+int main(int argc, char** argv)
 {
-    std::cout << "Loading Disease Simulation..." << std::endl;
+    MPI_Init(&argc, &argv);
+
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    if (rank == 0) {
+        std::cout << "Loading Disease Simulation on " << size << " processors..." << std::endl;
+    }
+
     Simulation sim = Simulation();
+
+    // Each process runs the simulation (could be parallelized inside the simulation itself)
     sim.start();
+
+    MPI_Finalize();
     return 0;
 }
+
+// int main()
+// {
+//     std::cout << "Loading Disease Simulation..." << std::endl;
+//     Simulation sim = Simulation();
+//     sim.start();
+//     return 0;
+// }
